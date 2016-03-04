08f1d760ad77f3f4ccbdd899bf0f93cf1568a08f
Matthew Thornton <matthewthornton.usa@gmail.com>
2016-03-04 11:03:03 -0800

    add math jax

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-/**
- * AngularJS Tutorial 1
- * @author Nick Kaye <nick.c.kaye@gmail.com>
- */
 
 /**
  * Main AngularJS Web Application
  */
-var app = angular.module('tutorialWebApp', [
+var app = angular.module('webApp', [
   'ngRoute'
 ]);
 
@@ -18,27 +14,17 @@ app.config(['$routeProvider', function ($routeProvider) {
     // Home
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
     // Pages
-    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
-    .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
-    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
-    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
-    // Blog
-    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
-    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
+    .when("/ratios_and_proportional_relationships", {templateUrl: "partials/ratios_and_proportional_relationships.html", controller: "PageCtrl"})
+    .when("/number_system", {templateUrl: "partials/number_system.html", controller: "PageCtrl"})
+    .when("/expressions_and_equations", {templateUrl: "partials/expressions_and_equations.html", controller: "PageCtrl"})
+    .when("/geometry", {templateUrl: "partials/geometry.html", controller: "PageCtrl"})
+    .when("/statistics_and_probability", {templateUrl: "partials/statistics_and_probability.html", controller: "PageCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
 
 /**
- * Controls the Blog
- */
-app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-  console.log("Blog Controller reporting for duty.");
-});
-
-/**
- * Controls all other Pages
+ * Controls Pages
  */
 app.controller('PageCtrl', function (/* $scope, $location, $http */) {
   console.log("Page Controller reporting for duty.");
